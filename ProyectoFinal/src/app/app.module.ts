d67a9676f0100450721444e9ba2bc1b2d493dbd5
Hidalgo Alexis Alejandro <alexishidale@gmail.com>
2022-06-10 03:08:48 -0300

    Personalización del cuerpo de la pagina Web
    "Agregamos gradiente al body de la pagina y creamos el componente banner"

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -6,13 +6,16 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
 import { SocialNetworksComponent } from './components/social-networks/social-networks.component';
+import { BannerComponent } from './components/banner/banner.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialNetworksComponent
+    SocialNetworksComponent,
+    BannerComponent,
   ],
   imports: [
     BrowserModule,
